8ea2d237faf24536d45b07c8815749a04d32dc35
Balla Gergely <b.gergely0407@gmail.com>
2024-03-20 11:58:58 +0100

    Updated Inventory and Door

--- src/main/java/logarlec/util/Door.java
+++ src/main/java/logarlec/util/Door.java
@@ -4,9 +4,25 @@ import logarlec.gameobjects.Person;
 import logarlec.gameobjects.Room;
 
 public class Door {
-	public void use(Person person, Room from) {}
+	private Room room1, room2; // can be used from room1 if one-way
+	private boolean oneWay;
+	public void use(Person person, Room from) {
+		if (!oneWay || from == room1) {
+			Room to = from == room1 ? room2 : room1;
+			if (to.enter(person)) {
+				from.leave(person);
+			}
+		}
+	}
 
-	public void move(Room from, Room to) {}
+	public void move(Room from, Room to) {
+		if (from == room1) {
+			room1 = to;
+		}
+		if (from == room2) {
+			room2 = to;
+		}
+	}
 
 	public void hide() {}
 
--- src/main/java/logarlec/util/Inventory.java
+++ src/main/java/logarlec/util/Inventory.java
@@ -4,14 +4,29 @@ import logarlec.gameobjects.Teacher;
 import logarlec.items.Item;
 import logarlec.gameobjects.Room;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+	private List<Item> items = new ArrayList<>();
+	private int itemCount = 0;
 	public boolean add(Item item) {
+		if (itemCount < 5) {
+			items.add(item);
+			return true;
+		}
 		return false;
 	}
 
-	public void remove(Item item) {}
+	public void remove(Item item) {
+		items.remove(item);
+	}
 
-	public void setRoom(Room room) {}
+	public void setRoom(Room room) {
+		items.forEach(i -> i.setRoom(room));
+	}
 
-	public void protectFrom(Teacher teacher) {}
+	public void protectFrom(Teacher teacher) {
+		items.forEach(i -> i.useAgainst(teacher));
+	}
 }
