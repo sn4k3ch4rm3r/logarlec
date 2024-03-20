436388bfa94a206a78529bed4c7f20cc942b96b1
Zelei Mátyás <zeleimatyi@gmail.com>
2024-03-20 16:10:21 +0100

    Started javadocs

--- src/main/java/logarlec/skeleton/TestCase.java
+++ src/main/java/logarlec/skeleton/TestCase.java
@@ -1,14 +1,31 @@
 package logarlec.skeleton;
 
+/**
+ * Abstract class for test cases
+ */
 public abstract class TestCase {
+    /**
+     * Name of the test case
+
+     */
     public String name;
 
+    /**
+     * Constructor for the test case
+     * @param name Name of the test case
+     */
     public TestCase(String name) {
         this.name = name;
     }
 
+    /**
+     * Abstract method to initialize the test case
+     */
     public abstract void init();
-    
+
+    /**
+     * Abstract method to run the test case
+     */
     public abstract void run();
 
 }
