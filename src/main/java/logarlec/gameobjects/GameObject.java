6d2ab48bd1225264fb5ad48bae37b9730ae0f179
Joób Zalán Miklós <joobzalan@gmail.com>
2024-03-20 11:54:38 +0100

    Added the necessary fields to the classes

--- src/main/java/logarlec/gameobjects/GameObject.java
+++ src/main/java/logarlec/gameobjects/GameObject.java
@@ -4,7 +4,12 @@ import logarlec.effects.Effect;
 import logarlec.items.Item;
 import logarlec.util.Updatable;
 
+import java.util.List;
+
 public abstract class GameObject implements Updatable {
+
+	List<Effect> effects;
+
 	public abstract void addItem(Item item);
 
 	public abstract void removeItem(Item item);
@@ -13,7 +18,11 @@ public abstract class GameObject implements Updatable {
 
 	public abstract void interactTeacher(Teacher teacher);
 
-	public void addEffect(Effect effect) {}
+	public void addEffect(Effect effect) {
+		effects.add(effect);
+	}
 
-	public void removeEffect(Effect effect) {}
+	public void removeEffect(Effect effect) {
+		effects.remove(effect);
+	}
 }
--- src/main/java/logarlec/gameobjects/Teacher.java
+++ src/main/java/logarlec/gameobjects/Teacher.java
@@ -1,11 +1,18 @@
 package logarlec.gameobjects;
 
 public class Teacher extends Person {
-	public void setPeaceful(boolean value) {}
+	boolean peaceful;
+	public void setPeaceful(boolean value) {
+		peaceful = value;
+	}
 
 	@Override
-	public void protectFromTeacher(Teacher target) {}
+	public void protectFromTeacher(Teacher target) {
+
+	}
 
 	@Override
-	public void interactTeacher(Teacher teacher) {}
+	public void interactTeacher(Teacher teacher) {
+
+	}
 }
