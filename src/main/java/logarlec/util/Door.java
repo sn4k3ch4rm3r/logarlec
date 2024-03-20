13919698b8f0b63935862397705ba6b506c676d4
Balla Gergely <b.gergely0407@gmail.com>
2024-03-20 14:15:09 +0100

    Added door constructor.

--- src/main/java/logarlec/util/Door.java
+++ src/main/java/logarlec/util/Door.java
@@ -6,6 +6,12 @@ import logarlec.gameobjects.Room;
 public class Door {
 	private Room room1, room2; // can be used from room1 if one-way
 	private boolean oneWay;
+
+	public Door(Room room1, Room room2) {
+		this.room1 = room1;
+		this.room2 = room2;
+	}
+
 	public void use(Person person, Room from) {
 		if (!oneWay || from == room1) {
 			Room to = from == room1 ? room2 : room1;
