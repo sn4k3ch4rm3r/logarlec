c704d8897024be9b3ecbfc14ba96730167b33c01
Balla Gergely <b.gergely0407@gmail.com>
2024-03-20 15:26:30 +0100

    Added logging.

--- src/main/java/logarlec/util/Door.java
+++ src/main/java/logarlec/util/Door.java
@@ -2,35 +2,48 @@ package logarlec.util;
 
 import logarlec.gameobjects.Person;
 import logarlec.gameobjects.Room;
+import logarlec.skeleton.Skeleton;
 
 public class Door {
 	private Room room1, room2; // can be used from room1 if one-way
 	private boolean oneWay;
 
 	public Door(Room room1, Room room2) {
+		Skeleton.createObject("door",Door.class,room1,room2);
 		this.room1 = room1;
 		this.room2 = room2;
+		Skeleton.logReturn(null);
 	}
 
 	public void use(Person person, Room from) {
+		Skeleton.logFunctionCall(this,"use",person,from);
 		if (!oneWay || from == room1) {
 			Room to = from == room1 ? room2 : room1;
 			if (to.enter(person)) {
 				from.leave(person);
 			}
 		}
+		Skeleton.logReturn(null);
 	}
 
 	public void move(Room from, Room to) {
+		Skeleton.logFunctionCall(this,"move");
 		if (from == room1) {
 			room1 = to;
 		}
 		if (from == room2) {
 			room2 = to;
 		}
+		Skeleton.logReturn(null);
 	}
 
-	public void hide() {}
+	public void hide() {
+		Skeleton.logFunctionCall(this,"hide");
+		Skeleton.logReturn(null);
+	}
 
-	public void show() {}
+	public void show() {
+		Skeleton.logFunctionCall(this,"show");
+		Skeleton.logReturn(null);
+	}
 }
--- src/main/java/logarlec/util/Inventory.java
+++ src/main/java/logarlec/util/Inventory.java
@@ -3,6 +3,7 @@ package logarlec.util;
 import logarlec.gameobjects.Teacher;
 import logarlec.items.Item;
 import logarlec.gameobjects.Room;
+import logarlec.skeleton.Skeleton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,22 +12,31 @@ public class Inventory {
 	private List<Item> items = new ArrayList<>();
 	private int itemCount = 0;
 	public boolean add(Item item) {
+		Skeleton.logFunctionCall(this,"add",item);
 		if (itemCount < 5) {
 			items.add(item);
+			Skeleton.logReturn(true);
 			return true;
 		}
+		Skeleton.logReturn(false);
 		return false;
 	}
 
 	public void remove(Item item) {
+		Skeleton.logFunctionCall(this,"remove",item);
 		items.remove(item);
+		Skeleton.logReturn(null);
 	}
 
 	public void setRoom(Room room) {
+		Skeleton.logFunctionCall(this,"setRoom",room);
 		items.forEach(i -> i.setRoom(room));
+		Skeleton.logReturn(null);
 	}
 
 	public void protectFrom(Teacher teacher) {
+		Skeleton.logFunctionCall(this,"protectFrom",teacher);
 		items.forEach(i -> i.useAgainst(teacher));
+		Skeleton.logReturn(null);
 	}
 }
