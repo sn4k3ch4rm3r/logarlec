6f693545b72b36d2a7d41203c548e970898c0b89
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2024-03-24 20:05:46 +0100

    Build using UTF-8

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 FOR /R "src" %%G IN (*.java) DO (
 	echo Building %%G
-	javac -classpath src/main/java;src/skeleton/java -d build\bin %%G
+	javac -encoding UTF-8 -classpath src/main/java;src/skeleton/java -d build\bin %%G
 )
 echo Build complete
