71f1b632a495ea03b33ddd5b914dd9adb0fad872
Balla Gergely <b.gergely0407@gmail.com>
2024-04-25 13:22:32 +0200

    Added MaskEffects running out of time

--- src/main/java/logarlec/effects/MaskEffect.java
+++ src/main/java/logarlec/effects/MaskEffect.java
@@ -2,6 +2,7 @@ package logarlec.effects;
 
 import logarlec.gameobjects.Student;
 import logarlec.gameobjects.Teacher;
+import logarlec.prototype.Prototype;
 
 public class MaskEffect extends Effect {
 
@@ -29,5 +30,17 @@ public class MaskEffect extends Effect {
 	public String toString() {
 		return String.format("MaskEffect <%d>\nHolder: <%d>\nTime remaining: %.0f\n",
 				this.hashCode(), this.holder.hashCode(), timeRemaining);
+	}
+	@Override
+	public void update(double deltaTime) {
+		super.update(deltaTime);
+		if (timeRemaining <= 0) {
+			holder.removeEffect(this);
+			try {
+				Prototype.out.write(String.format("<%d> ran out of time.\n", hashCode()).getBytes());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
