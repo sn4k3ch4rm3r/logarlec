e348faae13cb9e5d64855c213471f1ddda0c34c3
zal2004 <joobzalan@gmail.com>
2024-05-10 22:22:15 +0200

    First, ultra small mock-up

---
+++ src/main/java/logarlec/util/SpriteManager.java
@@ -0,0 +1,17 @@
+package logarlec.util;
+
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+
+public class SpriteManager {
+    private static Map<String, BufferedImage> spriteImages;
+
+    /**
+     * Betölti a megadott elérési útvonalon található sprite-okat.
+     * @param path Ha darabolós módszerrel töltünk, be akkor a fájl neve, különben a mappa neve.
+     */
+    public static void loadSprites(String path) {
+
+    }
+}
