2597b9996a9cdb916936ecf78c93e4112eb9f76f
zal2004 <joobzalan@gmail.com>
2024-05-17 08:26:49 +0200

    Revert "First mock-ups of the working of the map parsing."
    This reverts commit 80ca1bd9

--- src/main/java/logarlec/Configuration.java
+++ src/main/java/logarlec/Configuration.java
@@ -2,7 +2,4 @@ package logarlec;
 
 public final class Configuration {
 	public static final String SPRITE_PATH = "sprites.xml";
-	public static final String MAP_PATH = "map.xml";
-	public static final int MAP_WIDTH = 23;
-	public static final int MAP_HEIGHT = 24;
 }
