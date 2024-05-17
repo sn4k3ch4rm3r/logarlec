08ca31fedb1e585e72baa719fe1f94ae27904c81
Zelei Mátyás <zeleimatyi@gmail.com>
2024-05-17 12:52:40 +0200

    Added text drawing

--- src/main/java/logarlec/view/drawables/PlayerView.java
+++ src/main/java/logarlec/view/drawables/PlayerView.java
@@ -1,6 +1,7 @@
 package logarlec.view.drawables;
 
 import logarlec.model.gameobjects.Student;
+import logarlec.view.utils.TextRenderer;
 
 import java.awt.*;
 
@@ -18,9 +19,10 @@ public class PlayerView implements Drawable {
     public void draw(Graphics2D g2d) {
         int width = g2d.getClipBounds().width;
         int height = g2d.getClipBounds().height;
-        // Temp
-        g2d.drawString(name, 0, 0);
-        Graphics2D g = (Graphics2D) g2d.create(0, (int) (height * 0.4), 60, 60);
+        g2d.setColor(Color.WHITE);
+        Graphics2D g = (Graphics2D) g2d.create(0, 0, width, 40);
+        g.drawImage(TextRenderer.draw(name, 40), 0, 0, width, 40, null);
+        g = (Graphics2D) g2d.create(0, (int) (height * 0.4), 60, 60);
         personView.draw(g);
         g = (Graphics2D) g2d.create(60, (int) (height * 0.4), width - 60, 60);
         inventoryView.draw(g);
