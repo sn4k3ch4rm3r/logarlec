be7e3425774fda31951041d69960b8b3a873c905
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2024-05-22 05:21:08 +0200

    Updated the build script

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
-FOR /R "src" %%G IN (*.java) DO (
+FOR /R "src/main" %%G IN (*.java) DO (
 	echo Building %%G
-	javac -encoding UTF-8 -classpath src/main/java;src/prototype/java -d build\bin %%G
+	javac -encoding UTF-8 -classpath src/main/java -d build\bin %%G
 )
+echo Copying resources
+xcopy .\src\main\resources\ .\build\bin\ /E /I /Y
 echo Build complete
